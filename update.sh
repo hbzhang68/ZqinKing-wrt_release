43314cefb8a2a1bd0d1adc64ef1e5b1861e7998c
hbzhang68 <72781967+hbzhang68@users.noreply.github.com>
2025-04-07 15:52:53 +0800

    Update update.sh
    Signed-off-by: hbzhang68 <72781967+hbzhang68@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -90,6 +90,7 @@ remove_unwanted_packages() {
         "luci-app-ssr-plus" "luci-app-vssr" "luci-theme-argon" "luci-app-daed" "luci-app-dae"
         "luci-app-alist" "luci-app-argon-config" "luci-app-homeproxy" "luci-app-haproxy-tcp"
         "luci-app-openclash" "luci-app-mihomo" "luci-app-appfilter" "luci-app-msd_lite"
+        "luci-app-poweroff"
     )
     local packages_net=(
         "haproxy" "xray-core" "xray-plugin" "dns2socks" "alist" "hysteria"
@@ -155,7 +156,7 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        zerotier luci-app-zerotier msd_lite luci-app-msd_lite
+        zerotier luci-app-zerotier msd_lite luci-app-msd_lite luci-app-argon-config luci-app-poweroff
 }
 
 install_feeds() {
