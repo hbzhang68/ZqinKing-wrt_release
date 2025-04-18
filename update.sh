f62236c455b4336035619d1553749eeb4a1bfdeb
hbzhang68 <72781967+hbzhang68@users.noreply.github.com>
2025-04-18 16:55:47 +0800

    Update update.sh
    Signed-off-by: hbzhang68 <72781967+hbzhang68@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -150,13 +150,13 @@ update_golang() {
 install_small8() {
     ./scripts/feeds install -p small8 -f xray-core xray-plugin dns2tcp dns2socks haproxy hysteria \
         naiveproxy shadowsocks-rust sing-box v2ray-core v2ray-geodata v2ray-geoview v2ray-plugin \
-        tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev \
+        tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev luci-app-uhttpd \
         luci-app-passwall2 alist luci-app-alist smartdns luci-app-smartdns v2dat mosdns luci-app-mosdns \
         adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd \
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        zerotier luci-app-zerotier msd_lite luci-app-msd_lite luci-app-argon-config luci-app-poweroff
+        zerotier luci-app-zerotier msd_lite luci-app-msd_lite luci-app-argon-config luci-app-poweroff luci-app-upnp
 }
 
 install_feeds() {
