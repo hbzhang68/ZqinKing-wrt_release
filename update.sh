f6148b12ae32ac124681ef775227e7690dbde148
hbzhang68 <72781967+hbzhang68@users.noreply.github.com>
2025-07-30 21:19:10 +0800

    Update update.sh
    Signed-off-by: hbzhang68 <72781967+hbzhang68@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -962,6 +962,28 @@ remove_tweaked_packages() {
     fi
 }
 
+fix_gettext_compile_error() {
+    local patch_src="$BASE_PATH/patches/300-gettext-tools-define-bison-localedir.patch"
+    local gettext_path="$BUILD_DIR/package/libs/gettext-full"
+    local gettext_mk="$gettext_path/Makefile"
+    local patch_dest_dir="$gettext_path/patches"
+
+    if [ ! -f "$gettext_mk" ]; then
+        return
+    fi
+
+    local pkg_version
+    pkg_version=$(grep -oP 'PKG_VERSION:=\s*\K[0-9.]+' "$gettext_mk")
+
+    if [ "$pkg_version" = "0.24.1" ]; then
+        echo "gettext-full version is $pkg_version, applying patch."
+        # 从OpenWrt官方仓库获取最新的Makefile
+        curl -L -o "$gettext_mk" "https://raw.githubusercontent.com/openwrt/openwrt/main/package/libs/gettext-full/Makefile"
+        # 使用install命令简化补丁安装
+        install -Dm644 "$patch_src" "$patch_dest_dir/300-gettext-tools-define-bison-localedir.patch"
+    fi
+}
+
 main() {
     clone_repo
     clean_up
@@ -994,6 +1016,7 @@ main() {
     update_nss_diag
     update_menu_location
     fix_compile_coremark
+    fix_gettext_compile_error
     update_dnsmasq_conf
     add_backup_info_to_sysupgrade
     optimize_smartDNS
